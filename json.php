5af2a911779cb4ea946c96b0595e62a9d3210621
Nimitae <nimitae@hotmail.com>
2016-11-04 17:37:43 +0800

    test-graphs

--- json.php
+++ json.php
@@ -8,13 +8,11 @@ use GraphAware\Neo4j\Client\ClientBuilder;
 
 class Node
 {
-    var $id;
     var $group;
     var $name;
 
-    public function __construct($id, $group, $name)
+    public function __construct($name, $group)
     {
-        $this->id = $id;
         $this->group = $group;
         $this->name = $name;
     }
@@ -25,6 +23,7 @@ class Link
     var $source;
     var $target;
     var $value;
+
     public function __construct($source, $target, $value)
     {
         $this->source = $source;
@@ -53,20 +52,19 @@ $client = ClientBuilder::create()
 
 $query = "MATCH (c:Champion) RETURN c.champion_name, c.tag";
 $result = $client->run($query);
+$foodArray = array();
+$i = 0;
 foreach ($result->getRecords() as $record) {
-    $json_array["nodes"][] = new Node($record->value('c.champion_name'), $group_array[$record->value('c.tag')], $record->value('c.champion_name'));
+    $json_array["nodes"][] = new Node($record->value('c.champion_name'), $group_array[$record->value('c.tag')]);
+    $foodArray[$record->value('c.champion_name')] = $i++;
 }
-
 
 
 $query = "MATCH p=(c1: Champion)-[r:SAME_MATCH]-(c2: Champion)
-WHERE r.strength > 1000000
-RETURN c1.champion_name, c2.champion_name, r.strength;";
+WHERE r.compatibility >1
+RETURN c1.champion_name, c2.champion_name, r.compatibility LIMIT 1000;";
 $result = $client->run($query);
 foreach ($result->getRecords() as $record) {
-    $json_array["links"][] = new Link($record->value('c1.champion_name'), $record->value('c2.champion_name'), $record->value('r.strength')/1000000);
+    $json_array["links"][] = new Link($foodArray[$record->value('c1.champion_name')], $foodArray[$record->value('c2.champion_name')], 1);//$record->value('r.compatibility')/10000);
 }
 print json_encode($json_array);
-
-
-
---
+++ test-graph2.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<meta charset="utf-8">
+<style>
+
+    .node {
+        stroke: #fff;
+        stroke-width: 1.5px;
+    }
+
+    .link {
+        stroke: #999;
+        stroke-opacity: .6;
+    }
+
+</style>
+<body></body>
+<script src="http://d3js.org/d3.v2.min.js?2.9.6"></script>
+<script>
+    var width = 2000,
+        height = 1000;
+
+    var color = d3.scale.category20();
+
+    var force = d3.layout.force()
+        .charge(-50)
+        .linkDistance(30)
+        .size([width, height]);
+
+    var svg = d3.select("body").append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    d3.json("json.php", function (graph) {
+        force
+            .nodes(graph.nodes)
+            .links(graph.links)
+            .start();
+
+        var link = svg.selectAll(".link")
+            .data(graph.links)
+            .enter().append("line")
+            .attr("class", "link")
+            .style("stroke-width", function (d) {
+                return Math.sqrt(d.value);
+            });
+
+        var gnodes = svg.selectAll('g.gnode')
+            .data(graph.nodes)
+            .enter()
+            .append('g')
+            .classed('gnode', true);
+
+        var node = gnodes.append("circle")
+            .attr("class", "node")
+            .attr("r", 5)
+            .style("fill", function (d) {
+                return color(d.group);
+            })
+            .call(force.drag);
+
+        var labels = gnodes.append("text")
+            .text(function (d) {
+                return d.name;
+            });
+
+        console.log(labels);
+
+        force.on("tick", function () {
+            link.attr("x1", function (d) {
+                    return d.source.x;
+                })
+                .attr("y1", function (d) {
+                    return d.source.y;
+                })
+                .attr("x2", function (d) {
+                    return d.target.x;
+                })
+                .attr("y2", function (d) {
+                    return d.target.y;
+                });
+
+            gnodes.attr("transform", function (d) {
+                return 'translate(' + [d.x, d.y] + ')';
+            });
+
+
+        });
+    });
+
+</script>
