35a0b653ac04f71468dde010a1299afc9550a78b
minhthai <phammt@gmail.com>
2016-11-05 04:46:32 +0800

    Add user page.

---
+++ web/user.js
@@ -0,0 +1,67 @@
+"use strict";
+
+function showPage(page_index) {
+  $(".container-fluid.page-" + page_index).show();
+  $(".btn-page" + page_index).closest("li").addClass("active");
+}
+
+function hidePage(page_index) {
+  $(".container-fluid.page-" + page_index).hide();
+  $(".btn-page" + page_index).closest("li").removeClass("active");
+}
+
+function getCheckedValues(page_index) {
+  let checked = [];
+  $(".container-fluid.page-" + page_index + " div.checkbox input:checked").each(function(i) {
+    let value = $(this).val();
+    if(value) {
+      checked.push(value);
+    }
+  });
+  return checked;
+}
+
+function collectInfo() {
+  let info = {
+    "individuality": getCheckedValues(1),
+    "goals": getCheckedValues(2),
+    "notification": getCheckedValues(3)
+  };
+  console.log(info);
+  return info;
+}
+
+function sendRequest(url, obj) {
+  $.ajax(url, {
+    contentType: "application/json",
+    data: obj
+  }).done(function() {
+    console.log("Request sent to url.", obj);
+  });
+}
+
+$(document).ready(function () {
+  $(".btn-page1").click(function () {
+    showPage(1);
+    hidePage(2);
+    hidePage(3);
+    
+    sendRequest(url, collectInfo());
+  });
+
+  $(".btn-page2").click(function () {
+    hidePage(1);
+    showPage(2);
+    hidePage(3);
+    
+    sendRequest(url, collectInfo());
+  });
+
+  $(".btn-page3").click(function () {
+    hidePage(1);
+    hidePage(2);
+    showPage(3);
+    
+    sendRequest(url, collectInfo());
+  });
+});
